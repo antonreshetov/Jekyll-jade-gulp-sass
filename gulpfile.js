31453a06fc06f9aa566a2ad8d520e8c5f6cc8d89
antonreshetov <reshetov.art@gmail.com>
2016-03-22 18:10:50 +0300

    ---

--- gulpfile.js
+++ gulpfile.js
@@ -4,68 +4,68 @@ var sass        = require('gulp-sass');
 var prefix      = require('gulp-autoprefixer');
 var jade        = require('gulp-jade');
 var cp          = require('child_process');
-var watch 			= require('gulp-watch');
-var merge 			= require('merge-stream');
+var watch       = require('gulp-watch');
+var merge       = require('merge-stream');
 
 var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 var messages = {
-		jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
 
 /**
  * Build the Jekyll Site
  */
 gulp.task('jekyll-build', function (done) {
-		browserSync.notify(messages.jekyllBuild);
-		return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
-				.on('close', done);
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+        .on('close', done);
 });
 
 /**
  * Rebuild Jekyll & do page reload
  */
 gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-		browserSync.reload();
+    browserSync.reload();
 });
 
 /**
  * Wait for jekyll-build, then launch the Server
  */
 gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
-		browserSync({
-				server: {
-						baseDir: '_site'
-				}
-		});
+    browserSync({
+        server: {
+            baseDir: '_site'
+        }
+    });
 });
 
 /**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
 gulp.task('sass', function () {
-		return gulp.src('assets/_scss/main.scss')
-				.pipe(sass({
-						includePaths: ['scss'],
-						onError: browserSync.notify
-				}))
-				.pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-				.pipe(gulp.dest('_site/assets/css'))
-				.pipe(browserSync.reload({stream:true}))
-				.pipe(gulp.dest('assets/css'));
+    return gulp.src('assets/_scss/main.scss')
+        .pipe(sass({
+            includePaths: ['scss'],
+            onError: browserSync.notify
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('_site/assets/css'))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest('assets/css'));
 });
 
 /*
 * Jade + Multyply src & out
 */
 gulp.task('jade', function() {
-		var jadeInclude = gulp.src('_jade/include/*.jade')
-			.pipe(jade({pretty: true}))
-			.pipe(gulp.dest('_includes'));
-		var jadeLayouts = gulp.src('_jade/layout/*.jade')
-			.pipe(jade({pretty: true}))
-			.pipe(gulp.dest('_layouts'));
+    var jadeInclude = gulp.src('_jade/include/*.jade')
+      .pipe(jade({pretty: true}))
+      .pipe(gulp.dest('_includes'));
+    var jadeLayouts = gulp.src('_jade/layout/*.jade')
+      .pipe(jade({pretty: true}))
+      .pipe(gulp.dest('_layouts'));
 
-		return merge(jadeInclude, jadeLayouts);
+    return merge(jadeInclude, jadeLayouts);
 });
 
 /*
@@ -73,14 +73,14 @@ gulp.task('jade', function() {
 */
 
 gulp.task('copyfile', function() {
-		var img = gulp.src('assets/img/*.*')
-		.pipe(watch('assets/img/*.*'))
-		.pipe(gulp.dest('_site/assets/img'));
-		var js = gulp.src('assets/js/*.*')
-		.pipe(watch('assets/js/*.*'))
-		.pipe(gulp.dest('_site/assets/js'));
+    var img = gulp.src('assets/img/*.*')
+    .pipe(watch('assets/img/*.*'))
+    .pipe(gulp.dest('_site/assets/img'));
+    var js = gulp.src('assets/js/*.*')
+    .pipe(watch('assets/js/*.*'))
+    .pipe(gulp.dest('_site/assets/js'));
 
-		return merge(img, js);
+    return merge(img, js);
 });
 
 /**
@@ -88,9 +88,9 @@ gulp.task('copyfile', function() {
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-		gulp.watch('assets/_scss/**/*.scss', ['sass']);
-		gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
-		gulp.watch(['_jade/**/*.jade'], ['jade', 'jekyll-rebuild']);
+    gulp.watch('assets/_scss/**/*.scss', ['sass']);
+    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+    gulp.watch(['_jade/**/*.jade'], ['jade', 'jekyll-rebuild']);
 });
 
 /**
